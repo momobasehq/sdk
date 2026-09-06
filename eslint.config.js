280da88af8979316c820fb4c8438a838aed99069
Henry Hale <92443116+henryhale@users.noreply.github.com>
2026-09-06 12:59:23 +0300

    style: formatted with prettier

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "eslint/config";
 import tseslint from "typescript-eslint";
 
 export default defineConfig(
-	{ ignores: ["dist/**", "node_modules/**"] },
-	eslint.configs.recommended,
-	tseslint.configs.recommended,
+    { ignores: ["dist/**", "node_modules/**"] },
+    eslint.configs.recommended,
+    tseslint.configs.recommended
 );
